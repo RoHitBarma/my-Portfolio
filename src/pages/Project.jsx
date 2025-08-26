e6dcb4757b239dbf3ffa0eddaed13272cb7a1569
Rohit Barma <rohit@example.com>
2025-08-27 00:29:48 +0530

    updated portfolio with latest changes

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import portfolio from '../assets/project1.png'
+import BurgerStore from '../assets/project2.png'
+import BlogApp from '../assets/project3.png'
 
 const projects = [
   {
     title: "Portfolio Website",
-    description: "A responsive and animated personal portfolio built with React and Tailwind CSS.",
+    description: "A modern and fully responsive personal portfolio showcasing my skills, projects, and experience. Built with React, Tailwind CSS, and Framer Motion for smooth animations and an interactive UI.",
     tech: ["React", "Tailwind CSS", "Framer Motion"],
     image: portfolio, 
     live: "https://rohitbarma.github.io/my-Portfolio",
     github: "https://github.com/RoHitBarma/my-Portfolio",
   },
   {
-    title: "Weather App",
-    description: "Real-time weather data fetching app using OpenWeather API and React.",
-    tech: ["React", "CSS", "API"],
-    image: "/projects/weather.png",
-    live: "https://your-weather-app-link.com",
-    github: "https://github.com/yourusername/weather-app",
+    title: "Tasty Burger",
+    description: "A restaurant-themed website for a fictional burger brand. It includes a stylish homepage, menu section, brand details, and smooth navigation using React Router. Designed with Tailwind CSS for a modern and responsive look.",
+    tech: ["React", "Tailwind CSS", "React-Router"],
+    image: BurgerStore,
+    live: "https://rohitbarma.github.io/Tasty-Burger/",
+    github: "https://github.com/RoHitBarma/Tasty-Burger",
   },
   {
-    title: "E-commerce Store",
-    description: "Fully functional e-commerce website with cart and product filtering features.",
-    tech: ["React", "Tailwind CSS", "Node.js"],
-    image: "/projects/ecommerce.png",
-    live: "https://your-store-live-link.com",
-    github: "https://github.com/yourusername/ecommerce-store",
+    title: "Blog App",
+    description: "A mini blog application where users can explore posts with features like light/dark theme toggle, like/dislike functionality, and error handling using React Error Boundaries. Built with React and styled using Tailwind CSS and Bootstrap.",
+    tech: ["React", "Tailwind CSS", "Bootstrap"],
+    image: BlogApp,
+    live: "https://rohitbarma.github.io/blog-app/",
+    github: "https://github.com/RoHitBarma/blog-app",
   },
 ]
 
@@ -36,11 +38,11 @@ const Project = () => {
         My <span className="text-indigo-500">Projects</span>
       </h2>
 
-      <div>
+      <div className='grid gap-8 sm:grid-cols-2 lg:grid-cols-3'>
         {projects.map((project, index) => (
           <motion.div
             key={index}
-            className="bg-gray-800 rounded-xl shadow-lg overflow-hidden hover:scale-105 transform transition duration-300"
+            className="bg-gray-800 rounded-xl shadow-lg overflow-hidden transition duration-300"
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.4, delay: index * 0.1 }}
@@ -49,11 +51,13 @@ const Project = () => {
             <img
               src={project.image}
               alt={project.title}
-              className="w-full h-48 object-cover"
+              className="w-full h-48 object-cover transform hover:scale-105 transition duration-300"
             />
-            <div>
-              <h3>{project.title}</h3>
-              <p>{project.description}</p>
+
+            {/* card section */}
+            <div className='p-4'>
+              <h3 className='text-xl font-semibold mb-2'>{project.title}</h3>
+              <p className='text-sm text-gray-300 mb-4'>{project.description}</p>
 
               {/* Tech stack badges */}
               <div className="flex flex-wrap gap-2 mb-4">
